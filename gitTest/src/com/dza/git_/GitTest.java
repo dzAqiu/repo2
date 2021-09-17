99c883a9edfee0194b86361e457aae5835938a28
dingzhenao <1773901023@qq.com>
2021-09-17 11:16:51 +0800

    将Java项目提交到本地仓库

---
+++ gitTest/src/com/dza/git_/GitTest.java
@@ -0,0 +1,15 @@
+package com.dza.git_;
+
+/**
+ * @author 丁振奥~
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+    }
+}
